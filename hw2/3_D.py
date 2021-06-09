b2384e6fcefcd7ae5c13d3954c40063be462ea0d
Yaroslav_Lozinsky <lozinsky.yaroslav@gmail.com>
2021-06-09 22:23:12 +0300

    case of length 1 and 2

--- hw2/3_D.py
+++ hw2/3_D.py
@@ -1,16 +1,18 @@
 
 def more_than_neighbours(numbers):
     counter = 0
-    for i in range(1, len(numbers) - 1):
-        if numbers[i] > numbers[i - 1] and numbers[i] > numbers[i + 1]:
-            counter += 1
+    if len(numbers) >= 3:
+        for i in range(1, len(numbers) - 1):
+            if numbers[i] > numbers[i - 1] and numbers[i] > numbers[i + 1]:
+                counter += 1
     return counter
 
 
 assert more_than_neighbours([1, 2, 3, 4, 5]) == 0
 assert more_than_neighbours([5, 4, 3, 2, 1]) == 0
 assert more_than_neighbours([1, 5, 1, 5, 1]) == 2
-
+assert more_than_neighbours([1]) == 0
+assert more_than_neighbours([1, 2]) == 0
 
 def main():
     numbers = list(map(float, input().split()))
