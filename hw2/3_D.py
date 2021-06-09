527b20fe770e28150e74116450d98c3dbc480d11
Yaroslav_Lozinsky <lozinsky.yaroslav@gmail.com>
2021-06-09 22:17:34 +0300

    3_D initial commit

---
+++ hw2/3_D.py
@@ -0,0 +1,21 @@
+
+def more_than_neighbours(numbers):
+    counter = 0
+    for i in range(1, len(numbers) - 1):
+        if numbers[i] > numbers[i - 1] and numbers[i] > numbers[i + 1]:
+            counter += 1
+    return counter
+
+
+assert more_than_neighbours([1, 2, 3, 4, 5]) == 0
+assert more_than_neighbours([5, 4, 3, 2, 1]) == 0
+assert more_than_neighbours([1, 5, 1, 5, 1]) == 2
+
+
+def main():
+    numbers = list(map(float, input().split()))
+    answer = more_than_neighbours(numbers)
+    print(answer)
+
+if __name__ == '__main__':
+    main()
